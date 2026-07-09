1e1d48dbe5b3a4bb1fcd19a23f3955122f999ce1
Lokesh Reddyy <sailokesh365@gmail.com>
2026-07-09 19:41:01 +0530

    feat: implement JWT authentication and frontend auth pages

---
+++ backend/controllers/authController.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check existing user
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    // Encrypt password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+// Connect Database
+connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
+// Default Route
 app.get("/", (req, res) => {
   res.send("AI Textile Waste Intelligence Platform Backend Running 🚀");
 });
